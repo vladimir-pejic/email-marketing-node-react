e9b4e13698f5b1b8b2b725182d13ee6d769cfe5a
vladimir-pejic <vladimir.pejic@gmail.com>
2018-11-03 23:22:27 +0100

    mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect('mongodb://localhost:27017/emailer-dev', { useNewUrlParser: true });
 
 const app = express();
 
 require('./routes/auth')(app);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
