c7e02e6c572fd36a2f1ba1da81f0e68227269177
vladimir-pejic <vladimir.pejic@gmail.com>
2018-11-09 13:36:32 +0100

    fetch user client side, axios, actions

--- server/client/src/setupProxy.js
+++ server/client/src/setupProxy.js
@@ -2,4 +2,5 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     app.use(proxy('/auth/google', { target: 'http://127.0.0.1:3300' }))
+    app.use(proxy('/api/*', { target: 'http://127.0.0.1:3300' }))
 }
