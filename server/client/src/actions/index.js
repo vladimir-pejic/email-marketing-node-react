2de2282871f2690786f47479c429dbe77b474b4f
vladimir-pejic <vladimir.pejic@gmail.com>
2018-11-09 13:48:34 +0100

    fest user action asnyc/await refactor

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('/api/user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  }
+export const fetchUser = () => async dispatch =>  {
+  const res = await axios.get('/api/user');
+  dispatch({ type: FETCH_USER, payload: res });
 };
